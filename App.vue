ef6303e5226c820c12467accae58696c75312828
stefan <252556310@qq.com>
2019-10-25 18:23:11 +0800

    init commit

---
+++ App.vue
@@ -0,0 +1,27 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* 每个页面公共css */
+	/* uni官方ui库 */
+	@import "./common/uni.css";
+	/* 第三方动画样式库 */
+	@import "./common/animate.css";
+	/* 自定义图标库 */
+	@import "./common/icon.css";
+	/* UI基础库 */
+	@import "./common/zcm-main.css";
+	/* 自定义全局公共样式 */
+	@import "./common/common.css";
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<view class="d-flex bg-white border-top position-fixed bottom-0 left-0 right-0" style="height: 90upx;">
+			<view class="flex-1 d-flex j-center a-center flex-column line-h">
+				<view class="iconfont icon-xihuan line-h"></view>
+				收藏
+			</view>
+			<view class="flex-1 d-flex j-center a-center flex-column line-h">
+				<view class="iconfont icon-gouwuche line-h"></view>
+				购物车
+			</view>
+			<view class="text-white main-bg-color d-flex j-center a-center" style="flex: 2.5;" hover-class="main-bg-hover-color">加入购物车</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
